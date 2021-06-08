45428ddffabfd8db985ca548ec51157b8bf566bd
Mike Estes <sregge@gmail.com>
2021-06-07 23:56:09 -0400

    Begin tracking location, and displaying updates on the map.

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import {
+  Accuracy,
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+} from 'expo-location';
+
+export default (callback) => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        callback
+      );
+    } catch (err) {
+      setErr(err);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return [err];
+};
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,16 +1,27 @@
 // Import Dependencies
-import React from 'react';
+// import '../_mockLocation';
+import React, { useContext } from 'react';
 import { Text } from 'react-native-elements';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
 
 // Create component
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
+  const [err] = useLocation(addLocation);
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <Text h2>Create a Track</Text>
         <Map />
+        {err ? (
+          <Text style={styles.errorMessage}>
+            Please enable location services
+          </Text>
+        ) : null}
       </View>
     </SafeAreaView>
   );
@@ -21,6 +32,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Platform.OS === 'android' ? 50 : 0,
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: 'red',
+    marginLeft: 15,
+    marginTop: 15,
   },
 });
 
